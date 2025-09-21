a3077cd5cc2b527922fb522fa60e512893e8db94
BhaveshSabharwal <bhavesh.sabharwal.1@gmail.com>
2025-09-21 13:45:13 +0530

    added admin.ejs

--- src/server.js
+++ src/server.js
@@ -1,15 +1,15 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import ejs from 'ejs';
+import express from "express";
+import dotenv from "dotenv";
+import ejs from "ejs";
 import chalk from "chalk";
-import path from 'path';
+import path from "path";
 
-import { fileURLToPath } from 'url';
+import { fileURLToPath } from "url";
 
-import connectDB from '../config/db.js';
-import pageRoutes from './page routes/pages.js'; 
-import globalMiddlewares from './Middlewares/golbalMiddlewares.js';
-import authRouter from './routes/auth.js';
+import connectDB from "../config/db.js";
+import pageRoutes from "./page routes/pages.js";
+import globalMiddlewares from "./Middlewares/golbalMiddlewares.js";
+import authRouter from "./routes/auth.js";
 
 dotenv.config();
 
@@ -19,13 +19,12 @@ const PORT = process.env.PORT;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-
 // Set EJS as the templating engine
-app.set('views',path.join(__dirname,'./views'));
-app.set('view engine','ejs');
+app.set("views", path.join(__dirname, "./views"));
+app.set("view engine", "ejs");
 
 // accessing static files in public folder
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Global Middlewares
 globalMiddlewares(app);
@@ -34,14 +33,17 @@ globalMiddlewares(app);
 connectDB();
 
 // Pages and auth routes middleware
-app.use('/api', pageRoutes);
-app.use('/auth',authRouter);
+app.use("/api", pageRoutes);
+app.use("/auth", authRouter);
 
-app.get('/',(req,res)=>{
-    res.render('index.ejs');
+app.get("/", (req, res) => {
+  res.render("index.ejs");
 });
 
-app.listen(PORT,()=>{
-    console.log(chalk.blue(`Server is running on port ${PORT}`));
-})
+app.listen(PORT, () => {
+  console.log(chalk.blue(`Server is running on port ${PORT}`));
+});
 
+app.get("/error", (req, res) => {
+  res.status(404).render("error.ejs");
+});
