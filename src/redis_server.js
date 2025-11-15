6402a2c048b06ca09e2dab4b09b9a8e795bbb73f
BhaveshSabharwal <bhavesh.sabharwal.1@gmail.com>
2025-11-15 10:16:00 +0530

    fixed header and footer designs

--- src/redis_server.js
+++ src/redis_server.js
@@ -1,21 +1,25 @@
-import chalk from 'chalk';
-import { createClient } from 'redis';
+import chalk from "chalk";
+import { createClient } from "redis";
+import "dotenv/config";
+
+const REDIS_URL = process.env.REDIS_URL;
+const REDIS_PASSWORD = process.env.REDIS_PASSWORD;
+const REDIS_PORT = process.env.REDIS_PORT;
+const REDIS_USERNAME = process.env.REDIS_USERNAME;
 
 const client = createClient({
-    username: 'default',
-    password: 'ajJN0pRbs5cOZQiRSm0LnCmpEKqRjhGv',
-    socket: {
-        host: 'redis-13753.c10.us-east-1-2.ec2.redns.redis-cloud.com',
-        port: 13753
-    },
-    database:0
+  username: REDIS_USERNAME,
+  password: REDIS_PASSWORD,
+  socket: {
+    host: REDIS_URL,
+    port: Number(REDIS_PORT),
+  },
+  database: 0,
 });
 
-client.on('error', err => console.log('Redis Client Error', err));
+client.on("error", (err) => console.log("Redis Client Error", err));
 
 await client.connect();
 console.log(chalk.greenBright(" connected to redis ... "));
 
-
 export default client;
-
